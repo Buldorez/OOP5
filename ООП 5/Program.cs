98c172348283ac15693132df89e103f78cf749ec
Андрей Галеев <galeev.kamensk33@mail.ru>
2020-06-24 05:51:54 +0500

    Добавьте файлы проекта.

---
+++ ООП 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee tom = new Employee("Александр", 30, "Google");
+            Console.ReadLine();
+        }
+        class Person
+        {
+            string name;
+            int age;
+
+            public Person(string name)
+            {
+                this.name = name;
+                Console.WriteLine(this.name);
+            }
+            public Person(string name, int age) : this(name)
+            {
+                this.age = age;
+                Console.WriteLine(this.age);
+            }
+        }
+        class Employee : Person
+        {
+            string company;
+
+            public Employee(string name, int age, string company) : base(name, age)
+            {
+                this.company = company;
+                Console.WriteLine(name + " " + age + " лет" + " работает в " +company);
+            }
+        }
+    }
+}
